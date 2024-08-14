1b116ed70a5b96ad612f8fe1d781170207dea28c
LVXN456 <xuanbizet08@gmail.com>
2024-08-14 19:18:07 +0200

    sandbox

--- src/main/java/fr/lvxn/sandBox/SandBox.java
+++ src/main/java/fr/lvxn/sandBox/SandBox.java
@@ -1,5 +1,7 @@
 package fr.lvxn.sandBox;
 
+import fr.lvxn.sandBox.command.CommandBr;
+import org.bukkit.command.Command;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SandBox extends JavaPlugin {
@@ -7,7 +9,7 @@ public final class SandBox extends JavaPlugin {
     @Override
     public void onEnable() {
         System.out.println("SandBox Plugin Enabled");
-
+        getCommand("br").setExecutor(new CommandBr());
     }
 
     @Override
